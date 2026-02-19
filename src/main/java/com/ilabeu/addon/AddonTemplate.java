ed05727d5d62f5ac6a5647f3ded8477d1fe4263e
ilabeu <mionerandom55l@gmail.com>
2026-02-18 23:16:08 -0300

    Update AddonTemplate.java

--- src/main/java/com/ilabeu/addon/AddonTemplate.java
+++ src/main/java/com/ilabeu/addon/AddonTemplate.java
@@ -1,7 +1,11 @@
 package com.ilabeu.addon;
 
 import com.ilabeu.modules.ElytraFlyGrim;
+import com.ilabeu.commands.CommandExample;
+import com.ilabeu.hud.HudExample;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.commands.Commands;
+import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import org.slf4j.Logger;
@@ -14,7 +18,15 @@ public class AddonTemplate extends MeteorAddon {
     @Override
     public void onInitialize() {
         LOG.info("Inicializando Ilabeu Hax...");
+
+        // Modules
         Modules.get().add(new ElytraFlyGrim());
+
+        // Commands
+        Commands.get().add(new CommandExample());
+
+        // HUD
+        Hud.get().register(HudExample.INFO);
     }
 
     @Override
