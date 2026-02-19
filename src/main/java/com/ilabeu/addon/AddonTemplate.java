7f494c1eaab4e7e8cd049426a477611555d4b6b8
ilabeu <mionerandom55l@gmail.com>
2026-02-18 23:10:14 -0300

    Update AddonTemplate.java

--- src/main/java/com/ilabeu/addon/AddonTemplate.java
+++ src/main/java/com/ilabeu/addon/AddonTemplate.java
@@ -1,30 +1,20 @@
-package com.example.addon;
+package com.ilabeu.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ElytraFlyGrim;
+import com.ilabeu.modules.ElytraFlyGrim;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AddonTemplate extends MeteorAddon {
-    public static final Logger LOG = LoggerFactory.getLogger("AddonTemplate");
+    public static final Logger LOG = LoggerFactory.getLogger("IlabeuHax");
     public static final Category CATEGORY = new Category("Ilabeu Hax");
 
     @Override
     public void onInitialize() {
-        LOG.info("Inicializando Ilabeu Hax Addon...");
-
-        // Registra o seu novo módulo de Elytra para o GrimV3
+        LOG.info("Inicializando Ilabeu Hax...");
         Modules.get().add(new ElytraFlyGrim());
-
-        // Registra comandos e HUD (opcional, se você manteve os exemplos)
-        Commands.get().add(new CommandExample());
-        Hud.get().register(HudExample.INFO);
     }
 
     @Override
@@ -34,6 +24,6 @@ public class AddonTemplate extends MeteorAddon {
 
     @Override
     public String getPackage() {
-        return "com.example.addon";
+        return "com.ilabeu.addon";
     }
 }
